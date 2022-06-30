f55c9256f265f3a444859f61cb1bc26172a3a6f1
2i-DClegg <David.Clegg@2itesting.com>
2022-06-30 14:51:50 +0100

    Set up account navigation POM

---
+++ src/test/java/uk/co/twoitesting/POMPages/MyAccountPOM.java
@@ -0,0 +1,23 @@
+package uk.co.twoitesting.POMPages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import uk.co.twoitesting.utilities.SharedDictionary;
+
+public class MyAccountPOM {
+    WebDriver driver;
+
+    public MyAccountPOM(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(linkText = "Shop")
+    WebElement shopLink;
+
+    public void goToShop(){
+        shopLink.click();
+    }
+}
