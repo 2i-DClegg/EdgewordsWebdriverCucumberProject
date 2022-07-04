120ccad0bb9eb67e0778e83e1be00308ffeef2ab
2i-DClegg <David.Clegg@2itesting.com>
2022-07-04 16:04:22 +0100

    Improved teardown to clear old cart/discount code

--- src/test/java/uk/co/twoitesting/utilities/Hooks.java
+++ src/test/java/uk/co/twoitesting/utilities/Hooks.java
@@ -2,9 +2,16 @@ package uk.co.twoitesting.utilities;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.sl.In;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import uk.co.twoitesting.POMPages.CartPagePOM;
+
+import java.util.List;
 
 public class Hooks {
     private final SharedDictionary sharedDictionary;
@@ -22,8 +29,29 @@ public class Hooks {
     }
 
     @After
-    public void TearDown(){
+    public void TearDown()throws InterruptedException {
         WebDriver sharedDriver = (WebDriver) sharedDictionary.getValue("driver");
+        //Clear our cart
+
+
+        sharedDriver.findElement(By.linkText("Cart")).click();
+        //clear discount code
+        try{
+            sharedDriver.findElement(By.linkText("[Remove]")).click();
+        } catch (NoSuchElementException ignored) {
+
+        }
+
+        //clear all items in cart
+        try {
+           List<WebElement> removeButtons =  sharedDriver.findElements(By.className("remove"));
+            for(WebElement removeButton: removeButtons) {
+                removeButton.click();
+            }
+        } catch (NoSuchElementException ignored){
+
+        }
+
         sharedDriver.quit();
     }
 }
